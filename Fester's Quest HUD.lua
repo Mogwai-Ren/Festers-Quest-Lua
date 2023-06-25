e44ea9085784e37f3e6004e15fdeaa1f53e0f1ef
Mogwai_Ren <135550654+Mogwai-Ren@users.noreply.github.com>
2023-06-24 23:43:35 -0700

    Update Fester's Quest HUD.lua
    Figured out how item drops are stored in ram. Enemies and item drops should display correctly now.

--- Fester's Quest HUD.lua
+++ Fester's Quest HUD.lua
@@ -24,7 +24,18 @@ local slotIdLookup = {
     [0x13] = "FaceMove",
     [0x14] = "GreenRock",
     [0x16] = "EyeBall",
-    [0x20] = "Item", -- Can't find how items are stored yet. 0x03*6 & 0x03*7 & 0x03*E?
+    [0x20] = "Item", 
+}
+
+-- Defines the item drop lookup table
+local itemDropLookup = {
+    [0x00] = "Key",
+    [0x01] = "Money",
+    [0x02] = "Lightbulb",
+    [0x03] = "GunUp",
+    [0x04] = "WhipUp",
+    [0x05] = "GunDown",
+    [0x06] = "WhipDown",
 }
 
 -- Non Boss Memory Slots 
@@ -34,7 +45,7 @@ local slotIdLookup = {
 		-- 0x03*2 ??
 		-- 0x03*3 Slot X 
 		-- 0x03*4 Visible on screen
-		-- 0x03*5 Jumping/Floating?
+		-- 0x03*5 Jumping/*Item DropID if (slotIdLookup == 20 and 0x03*5 == (itemDropLookup))**
 		-- 0x03*6 ?? 
 		-- 0x03*7 ??
 		-- 0x03*8 Slot Health
@@ -67,7 +78,7 @@ function displayInfo()
     		-- Noose 0x015E
     		-- Vice Grips 0x015F 
     		-- Potion 0x0160 
-    		-- Invisible 0x0161 
+    		-- Invisible 0x0161, Invisible timer 0x00CC
     		-- Money 0x162 
     		-- Missile 0x0163 
     		-- TNT 0x0164
@@ -81,20 +92,20 @@ function displayInfo()
     -- Gets Fester's Key Count, Gun Level, GPS, "Step" from RAM addresses
     local keyCount = emu.read(0x015D, emu.memType.nesDebug) 
     local gunLevel = emu.read(0x04E7, emu.memType.nesDebug)
-    local festerStep = emu.read(0x04F7, emu.memType.nesDebug)
+    --local festerStep = emu.read(0x04F7, emu.memType.nesDebug)
   
     	-- Gets festerX:Y ** Not sure correct address; using for now **
     --local festerX = emu.read(0x04F3, emu.memType.nesDebug) 
     --local festerY = emu.read(0x04F9, emu.memType.nesDebug) 
 
     	-- Displays Key, Gun, Step
-    emu.drawString(140, 0, "Key:" .. (keyCount - 1), 0xFFFFFF, 0xFF000000) --140
-    emu.drawString(140, 8, "Gun:" .. gunLevel, 0xFFFFFF, 0xFF000000)
-    emu.drawString(140, 16, "Step:" .. festerStep, 0xFFFFFF, 0xFF000000)
+    emu.drawString(140, 0, "Key:" .. (keyCount - 1), 0xFFFFFF, 0xFF000000) -- 140
+    emu.drawString(140, 8, "Gun:" .. gunLevel, 0xFFFFFF, 0xFF000000) -- 140
+    --emu.drawString(140, 16, "Step:" .. festerStep, 0xFFFFFF, 0xFF000000) -- 140
     
     	-- GPS
-    --emu.drawString(100, 0, 'X:' .. festerX, 0xFFFFFF, 0xFF000000)
-    --emu.drawString(100, 8, "Y:" .. festerY, 0xFFFFFF, 0xFF000000)
+    --emu.drawString(100, 0, 'X:' .. festerX, 0xFFFFFF, 0xFF000000) -- 100
+    --emu.drawString(100, 8, "Y:" .. festerY, 0xFFFFFF, 0xFF000000) -- 100
 
     -- Display mSlot(Enemy) Health
     for address, mSlot in pairs(slotHealthLookup) do
@@ -102,16 +113,24 @@ function displayInfo()
         emu.drawString(172, (mSlot - 1) * 8, mSlot .. ":" .. slotHealth, 0xFFFFFF, 0xFF000000)
     end
 
-    -- Displays SlotIdValue
-    for slotNumber, slotIdValue in ipairs(slotIdValues) do
-        -- Only displays slot ID if the check value is above 0
-        if checkValues[slotNumber] > 0 then
-            -- Gets the slot IDs from the lookup table
-            local slotId = slotIdLookup[slotIdValue] or "Null"
-            -- Prints the slot IDs on the screen
-            emu.drawString(202, (slotNumber - 1) * 8, slotId, 0xFFFFFF, 0xFF000000)
-        end
-    end
+    	-- Displays SlotIdValue and ItemDropId
+	for slotNumber, slotIdValue in ipairs(slotIdValues) do
+    	-- Only displays slot ID if the check value is above 0
+    	if checkValues[slotNumber] > 0 then
+        	-- Gets the slot IDs from the lookup tables
+        local slotId
+		if slotIdValue == 0x20 then
+           	-- Retrieve item drop ID using the itemDropLookup table
+			local itemDropId = emu.read(0x0305 + 0x10 * (slotNumber - 1), emu.memType.nesDebug)
+			slotId = itemDropLookup[itemDropId] or "Null"
+			else
+	        	-- Retrieve regular slot ID using the slotIdLookup table
+          	slotId = slotIdLookup[slotIdValue] or "Null"
+        	end
+	        -- Prints the slot IDs on the screen
+	        emu.drawString(202, (slotNumber - 1) * 8, slotId, 0xFFFFFF, 0xFF000000) -- 202
+		end
+	end
 end
 
 emu.addEventCallback(displayInfo, emu.eventType.endFrame)
